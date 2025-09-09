51e0ba0d4bcbc5363b9e89dc7c7b2edf780a5f62
Markus <markusgutierrez10@gmail.com>
2025-09-09 14:39:07 -0500

    creacion vista initial

--- perfil/lib/main.dart
+++ perfil/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 import 'presentation/view/cargando.dart';
 import 'presentation/view/failure.dart';
 
@@ -15,7 +16,42 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
+        backgroundColor: const Color.fromARGB(255, 187, 208, 224),
+        body: Center(
+         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Bienvenido",
+              style: TextStyle(
+                fontSize: 46,
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                  offset: Offset(2, 2),
+                  blurRadius: 3,
+                  color: const Color.fromARGB(255, 100, 96, 96)
+                )]
+              ),
+            ),
+            SizedBox(height: 20),
+            Icon(
+                Icons.waving_hand,
+                size: 100,
+                color: Colors.blue,
+              ),
+            SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {
+                print('Botón presionado');
+              },
+              child: Text('Empezar'),
+            )
+          ],
+         ),
+        ),
       ),
     );
   }
--- perfil/lib/presentation/view/cargando.dart
+++ perfil/lib/presentation/view/cargando.dart
@@ -1,17 +1,34 @@
-
 import 'package:flutter/material.dart';
 
 class Loading extends StatelessWidget {
-  const Loading({
-    super.key,
-  });
+  const Loading({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 64, 63, 78),
-      body: Center(
-        child: Image.asset("assets/cargando.gif"),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/cargando.gif",),
+              Text("Cargando...",
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 32,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(2, 2),
+                      blurRadius: 4,
+                      color: const Color.fromARGB(255, 73, 70, 70)
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
